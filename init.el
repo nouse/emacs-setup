d74e564a3d228d6922397cfebd482f87674caf2d
Jiang Wu(Mike) <wu.jiang@mozy.com>
2013-05-22 13:47:57 +0800

    use marmalade for stable version

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 ;; evil
